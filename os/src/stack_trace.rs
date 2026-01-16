6f5f35dff60efbc2a9b5877015ecfd34b7d7226a
anonymouy <tangyn23@mail2.sysu.edu.cn>
2026-01-16 12:22:03 +0800

    some changes...

---
+++ os/src/stack_trace.rs
@@ -0,0 +1,25 @@
+//! print stack trace
+
+// this is a line comments
+/*
+  this is a block omments
+*/
+
+///  this is a documen line comments
+use core::{arch::asm, ptr};
+
+pub unsafe fn print_stack_trace() -> () {
+    let mut fp: *const usize;
+    asm!("mv {}, fp", out(reg) fp);
+
+    println!("== Begin stack trace ==");
+    while !fp.is_null() {
+        let saved_ra = *fp.sub(1);
+        let saved_fp = *fp.sub(2);
+
+        println!("0x{:016x}, fp = 0x{:016x}", saved_ra, saved_fp);
+
+        fp = saved_fp as *const usize;
+    }
+    println!("== End stack trace ==");
+}
