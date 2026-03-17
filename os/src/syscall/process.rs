b9b2c58f11e8aed66a9b06f8eec47f81f260bfc6
anonymouy <tangyn23@mail2.sysu.edu.cn>
2026-03-17 17:58:34 +0800

    first commit

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,9 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use core::ptr::write_volatile;
+
+use alloc::vec::Vec;
+
+use crate::{mm::{VirtAddr, translated_byte_buffer}, task::{change_program_brk, current_user_token, exit_current_and_run_next, suspend_current_and_run_next}, timer::get_time_us};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -27,14 +31,45 @@ pub fn sys_yield() -> isize {
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let user_token = current_user_token();
+    // 由于指令对齐，因此我们不必担心usize数据被两页截断。
+    let v = translated_byte_buffer(user_token, _ts as *const u8, core::mem::size_of::<TimeVal>());
+    let time = get_time_us();
+    let sec = time / 1_000_000;
+    let usec = time % 1_000_000;
+    // 2. 将两个 usize 转化为底层的字节数组 (使用本地字节序 native endian)
+    let mut time_bytes = [0u8; 16]; // RV64 下两个 usize 正好是 16 字节
+    time_bytes[0..8].copy_from_slice(&sec.to_ne_bytes());
+    time_bytes[8..16].copy_from_slice(&usec.to_ne_bytes());
+
+    // 3. 像倒水一样，把这 16 个字节依次倒进 v 提供的一个或两个物理切片中
+    let mut offset = 0;
+    for slice in v {
+        let len = slice.len();
+        // 从 time_bytes 中截取对应长度的数据，复制到物理页切片中
+        slice.copy_from_slice(&time_bytes[offset .. offset + len]);
+        offset += len; // 移动水位线
+    }
+    0
 }
 
 /// TODO: Finish sys_trace to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 => unsafe {
+            *(_id as *const isize)
+        }
+        1 => {
+            unsafe { (_id as *mut u8).write_volatile(_data as u8) }; 
+            0
+        },
+        2 => {
+            get_syscall_times(_id)
+        },
+        _ => panic!("invalid trace request")
+    }
 }
 
 // YOUR JOB: Implement mmap.
